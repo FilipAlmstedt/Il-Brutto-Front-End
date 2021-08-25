6e94289524cbd4ce1f5a35c8fd99818cbd608d3c
FilipAlmstedt <filip.almstedt@almxer.se>
2021-08-25 16:29:27 +0200

    Added primary styling amd create navbar component

---
+++ src/Components/Layout/Navbar.tsx
@@ -0,0 +1,26 @@
+import { Link } from "react-router-dom";
+
+export const Navbar = () => {
+    return (
+        <>
+            <div className="navbarContainer">
+                <nav>
+                    <ul className="navbarUl">
+                        <li>
+                            <Link to="/">
+                                <div className="logoContainer">
+                                    <b><p>LOGO</p></b>     
+                                </div>
+                            </Link>
+                        </li>
+                        <div className="notLogoLinks">
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to="/menu">Menu</Link></li>
+                            <li><Link to="/booking">Booking</Link></li>
+                        </div>
+                    </ul>
+                </nav>
+            </div>
+        </>
+    );
+}
