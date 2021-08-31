9ef19db1eb607053758552c65db59475a4560239
erik åström <erik.astrom2@gmail.com>
2021-08-31 16:19:00 +0200

    userCalender functionality beta

--- src/Components/BookingComponents/CalendarPlugin.tsx
+++ src/Components/BookingComponents/CalendarPlugin.tsx
@@ -1,32 +1,43 @@
- import { useState } from "react";
+import { useState, ChangeEvent } from "react";
 import Calendar from "react-calendar";
-import 'react-calendar/dist/Calendar.css';
+import "react-calendar/dist/Calendar.css";
 
 interface ICalendarPluginProps {
-  getUserInput(chosenDate: Date, guestAmount: number): void
+  getUserInput(chosenDate: Date, guestAmount: number): void;
 }
+
+// interface IState {
+//   guestInput: {
+//     date:Date,
+//     guestAmount:number
+//   }
+// }
 
 export function CalendarPlugin(props: ICalendarPluginProps) {
   const [chosenDate, setChosenDate] = useState(new Date());
-  const [guestAmount, setGuestAmount] = useState(1);
-  // Test string
-  let selectedDate = JSON.stringify(chosenDate);
-  //console.log(chosenDate);
-  
+
+  // const [guestInput, setGuestinput] = useState<IState>();
+
+  const submitGuestAmount = (e: ChangeEvent<HTMLInputElement>) => {
+    // setGuestAmont(+e.target.value)
+    props.getUserInput(chosenDate, +e.target.value);
+  };
 
   return (
     <>
       <h1>CalendarPlugin works!</h1>
       <div style={{ width: 700 }}>
+        <label htmlFor="guestAmount">Guest amount: </label>
+        <input
+          id="guestAmount"
+          onChange={submitGuestAmount}
+          type="number"
+          defaultValue={1}
+          min={1}
+          placeholder="Type in amount of guests"
+        />
 
-          
-          <label htmlFor="numberOfGuests">Number of guests: </label>
-          {/*Collect number of guests and date from calendar plugin and send it to a different function to get results from DB*/}
-          <input type="number" id="numberOfGuests" onChange={event => setGuestAmount(+event.target.value)} min="1"/>
-          <Calendar onChange={setChosenDate} showWeekNumbers value={chosenDate} />
-      
-        <p> You selected {selectedDate}</p>
-        <p> You selected {guestAmount}</p>
+        <Calendar onChange={setChosenDate} showWeekNumbers value={chosenDate} />
       </div>
     </>
   );
--- src/Components/BookingPage.tsx
+++ src/Components/BookingPage.tsx
@@ -4,32 +4,61 @@ import { UserForm } from "./BookingComponents/UserForm";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { Booking } from "../Models/Booking";
+import Moment from 'react-moment';
+import moment from "moment";
 
 export const BookingPage = () => {
   const [bookings, setBookings] = useState<Booking[]>([]);
-  const getBookings = (chosenDate: Date, guestAmount: number) => {};
+  // let takenTables:number = 15;
+  const getBookings = (chosenDate: Date, guestAmount: number) => {
+    let currentBookings:Booking[] = [];
+    let earlyBookings:Booking[] = [];
+    let lateBookings:Booking[] = [];
+    axios
+      .get<Booking[]>("http://localhost:8000/reservations")
+      .then((response)=> {
+        
+       for (let i = 0; i < response.data.length; i++) {
+        let dbDate:Date = response.data[i].date;
+        
+        if(moment(chosenDate).format("YYYY MM DD") === moment(dbDate).format("YYYY MM DD")){
+
+          currentBookings.push(response.data[i])
+          
+        }
+       }
+       
+       for (let i = 0; i < currentBookings.length; i++) {
+         (currentBookings[i].seatingTime === 'early') ? earlyBookings.push(currentBookings[i]) : lateBookings.push(currentBookings[i])
+         
+       }
+       console.log("early:", earlyBookings.length, "late:", lateBookings.length);
+       
+
+      })
+
+  };
 
   useEffect(() => {
     axios
       .get<Booking[]>("http://localhost:8000/reservations")
       .then((response) => {
-        console.log(response.data);
+
         setBookings(response.data);
       });
   }, []);
 
-  let liTags = bookings.map((booking) => {
-    return (
-      <li key={booking.bookingRef}>
-        <h1>{booking.seatingTime}</h1>
-      </li>
-    );
-  });
+
 
   return (
     <>
       <CalendarPlugin getUserInput={getBookings}></CalendarPlugin>
-      <ul>{liTags}</ul>
+      {/* { 
+        (takenTables > 14)
+          ? <div> Fullbooked </div> 
+          : <div> 18.00 </div> 
+      } */}
+
       {/* <UserForm></UserForm>
       <BookingSummary></BookingSummary> */}
     </>
