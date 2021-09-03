1c36b9e17e6f7b3565dd357197888a20c4393ed0
edvinsjogren <edvin.sjogren@gmail.com>
2021-09-03 15:59:13 +0200

    added post button and functionality to booking page. ISSUE: need to fix redirect to confirmation page

--- src/Components/BookingComponents/CalendarPlugin.tsx
+++ src/Components/BookingComponents/CalendarPlugin.tsx
@@ -1,5 +1,5 @@
 import { useState, ChangeEvent, useEffect } from "react";
-import Calendar, { OnChangeDateCallback } from "react-calendar";
+import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 
 interface ICalendarPluginProps {
