1036882806ea119bde7802a215450322c779ab26
fpasth <filip.almstedt@almxer.se>
2021-09-07 15:11:40 +0200

    commit

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
@@ -7,3 +6,20 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+// Create asyncronic test
+
+/* import axios from 'axios'; */
+// jest.mock("axios");
+// const mockAxios = axios as jest.Mocked<typeof axios>;
+
+// const mockData: IMovie[] = [{...}, {...}];
+/*test('async testing', () => {
+  mockAxios.get.mockResolvedValues({data: mockData}); // När axios gör en get, skicka tillbaka mockdata
+  
+  render(<Movies />);
+  await waitFor(() => {
+    const movies = screen.getAllByRole("heading"); // Hämta alla headings
+    expect(movies.length).toBe(mockData.length);
+  })
+}) */
