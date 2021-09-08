19961b986d7b94259141f3a878d8dd4122296df2
edvinsjogren <edvin.sjogren@gmail.com>
2021-09-09 00:32:45 +0200

    added icons including mixins. also added desktop css rules

--- src/Components/Layout/Footer.tsx
+++ src/Components/Layout/Footer.tsx
@@ -7,35 +7,35 @@ export const Footer = () => {
         <ul className="social-media-links">
           <li>
             <a href="www.facebook.com">
-              <div className="icon-facebook">Facebook</div>
+              <div className="icon-facebook"></div>
             </a>
           </li>
           <li>
             <a href="www.instagram.com">
-              <div className="icon-instagram">Instagram</div>
+              <div className="icon-instagram"></div>
             </a>
           </li>
           <li>
             <a href="www.tripadvisor.com">
-              <div className="icon-tripadvisor">Tripadvisor</div>
+              <div className="icon-tripadvisor"></div>
             </a>
           </li>
         </ul>
 
         <ul className="contact-information">
           <li>
-            <span className="icon-clock"></span> tue - sat, 18:00 - late
+            <div className="icon-clock"> </div> tue - sat, 18:00 - late
           </li>
           <li>
-            <span className="icon-pin"></span> Italienska torget 123 - 123 45
+            <div className="icon-pin"> </div> Italienska torget 123 <br/> 123 45
             Stockholm
           </li>
           <li>
-            <span className="icon-tel"></span> 08 123 45 67
+            <div className="icon-tel"> </div> 08 123 45 67
           </li>
         </ul>
 
-        <p> All rights reserved - IL BRUTTO TM</p>
+        <p> All rights reserved - IL BRUTTO ™</p>
       </div>
     </>
   );
