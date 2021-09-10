ddb825472325fe709945feb676b728ba8fbfaec4
fpasth <filip.almstedt@almxer.se>
2021-09-10 10:10:20 +0200

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -8,46 +8,6 @@ import { AdminPage } from "./Components/Pages/AdminPage";
 import { PageNotFound } from "./Components/Pages/PageNotFound";
 import { Navbar } from "./Components/Layout/Navbar";
 import { Footer } from "./Components/Layout/Footer" 
-<<<<<<< HEAD
-
-function App() {
-  return (
-    <div className="App">
-      <Router>
-    
-      <Navbar></Navbar>
-
-      <Switch>
-        <Route exact path="/">
-          <HomePage></HomePage>
-        </Route>
-
-        <Route path="/menu">
-          <MenuPage></MenuPage>
-        </Route>
-
-        <Route path="/booking">
-          <BookingPage></BookingPage>
-        </Route>
-
-        <Route path="/confirmation/:id">
-          <ConfirmationPage></ConfirmationPage>
-        </Route>
-
-        <Route path="/admin">
-          <AdminPage></AdminPage>
-        </Route>
-
-        <Route path="*">
-          <PageNotFound></PageNotFound>
-        </Route>
-      </Switch>
-
-      <Footer></Footer>
-
-    </Router>
-    </div>
-=======
 import { AdminEditBooking } from "./Components/AdminComponents/AdminEditBooking";
 import { AnimatePresence } from "framer-motion";
 
@@ -94,7 +54,6 @@ function App() {
       </Router>
       </div>
     </AnimatePresence>
->>>>>>> 38aa64fe49478f49b0bcf4d745f0cd551274fd1c
   );
 }
 
